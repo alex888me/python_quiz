7a939ba4f311b977ec7c90e58cb4dc68d46b7acb
art <art971m@gmail.com>
2024-08-04 20:04:17 +0200

    2 questions

--- home/management/commands/add_questions.py
+++ home/management/commands/add_questions.py
@@ -6,15 +6,39 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         # Create a type
-        gfg_type = Types.objects.create(gfg_name='General Knowledge')
+        gfg_type = Types.objects.create(gfg_name='Python Essentials 1')
 
-        # Create a question
-        question = Question.objects.create(gfg=gfg_type, question='What is the capital of France?', marks=5)
 
-        # Create answers
-        Answer.objects.create(question=question, answer='Paris', is_correct=True)
-        Answer.objects.create(question=question, answer='Berlin', is_correct=False)
-        Answer.objects.create(question=question, answer='Madrid', is_correct=False)
-        Answer.objects.create(question=question, answer='Rome', is_correct=False)
+        question = Question.objects.create(
+            gfg=gfg_type,
+            question=r'The \n digraph forces the print() function to:', marks=5)
+        Answer.objects.create(question=question, answer=r'output exactly two characters: \ and n', is_correct=False)
+        Answer.objects.create(question=question, answer=r'break the output line', is_correct=True)
+        Answer.objects.create(question=question, answer=r'duplicate the character next to the digraph', is_correct=False)
+        Answer.objects.create(question=question, answer=r'stop its execution', is_correct=False)
+
+        question = Question.objects.create(
+            gfg=gfg_type,
+            question="What is the output of the following snippet?\n"
+                     "z = y = x = 1\n"
+                     "print(x, y, z, sep='*')", marks=5)
+        Answer.objects.create(question=question, answer=r'x*y*z', is_correct=False)
+        Answer.objects.create(question=question, answer=r'1 1 1', is_correct=False)
+        Answer.objects.create(question=question, answer=r'x y z', is_correct=False)
+        Answer.objects.create(question=question, answer=r'1*1*1', is_correct=True)
+
+
+        """
+        Python Essentials 1    Module 2
+        Data types, variables, basic input-output operations, basic operators    
+        how to write and run simple Python programs;
+        what Python literals, operators, and expressions are;
+        what variables are and what are the rules that govern them;
+        how to perform basic input and output operations.
+        
+        python manage.py migrate
+        python manage.py runserver
+        python manage.py add_questions
+        """
 
         self.stdout.write(self.style.SUCCESS('Successfully added questions and answers'))
